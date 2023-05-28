d8a4997a2ed5f3b8135294540f2ac54b1c5c3234
MatheusGorga <matheusgobar@gmail.com>
2023-05-28 19:24:35 -0300

    banco

--- src/main/java/br/com/alura/screenmatch/domain/filme/Filme.java
+++ src/main/java/br/com/alura/screenmatch/domain/filme/Filme.java
@@ -1,36 +1,73 @@
 package br.com.alura.screenmatch.domain.filme;
 
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "filmes")
 public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Nullable
     private String nome;
+
+    @Column
+    @Nullable
     private Integer duracaoEmMinutos;
+
+    @Column
+    @Nullable
     private Integer anoLancamento;
+
+    @Column
+    @Nullable
+
     private String genero;
 
-    public Filme(DadosCadastroFilme dados){
+    public Filme(DadosCadastroFilme dados) {
         this.nome = dados.nome();
         this.duracaoEmMinutos = dados.duracao();
         this.anoLancamento = dados.ano();
         this.genero = dados.genero();
     }
 
-    
+    public Filme(){
+        
+    }
+
     public String getNome() {
         return nome;
     }
+
     public Integer getDuracaoEmMinutos() {
         return duracaoEmMinutos;
     }
+
     public Integer getAnoLancamento() {
         return anoLancamento;
     }
+
     public String getGenero() {
         return genero;
     }
+
     @Override
     public String toString() {
         return "Filme [nome=" + nome + ", duracaoEmMinutos=" + duracaoEmMinutos + ", anoLancamento=" + anoLancamento
                 + ", genero=" + genero + "]";
     }
 
+    public Long getId() {
+        return id;
+    }
 
 }
